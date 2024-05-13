f2c95a707046e1994a4ace30f02b1a221b8c6b9e
Federico Capaldo <federico.capaldo@hotmail.com>
2024-05-13 21:36:30 +0200

    performing ajax request on submit form, instead of refreshing page, and console logging result from update function that returns a plain text partial with the movie updated

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,7 +17,11 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
 
     @movie.update(movie_params)
-    redirect_to movies_path
+
+    respond_to do |format|
+      format.html { redirect_to movies_path }
+      format.text { render partial: "movie_infos", locals: { movie: @movie }, formats: [:html] }
+    end
   end
 
   private
--- app/javascript/controllers/edit_movie_controller.js
+++ app/javascript/controllers/edit_movie_controller.js
@@ -12,4 +12,19 @@ export default class extends Controller {
     this.infosTarget.classList.add("d-none")
     this.formTarget.classList.remove("d-none")
   }
+
+  update(event) {
+    event.preventDefault() // stops from refreshing the page
+
+    const url = this.formTarget.action
+    fetch(url, {
+      headers: { "Accept": "text/plain" },
+      method: "PATCH",
+      body: new FormData(this.formTarget)
+    })
+      .then(response => response.text())
+      .then(data => {
+        console.log(data)
+      })
+  }
 }
