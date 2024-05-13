1a9378ef500fce9a47075b663a7c5f07a0d27c30
Federico Capaldo <federico.capaldo@hotmail.com>
2024-05-13 21:23:54 +0200

    added basic update controller method which actually updates movie upon submitting the edit form already

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,4 +12,17 @@ class MoviesController < ApplicationController
       format.text { render partial: "movies/list", locals: { movies: @movies }, formats: [:html] }
     end
   end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+    @movie.update(movie_params)
+    redirect_to movies_path
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :year)
+  end
 end
