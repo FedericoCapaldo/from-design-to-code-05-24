eb55fb1b28107fc7f2148d5e5709aa69a6c22d6c
Federico Capaldo <federico.capaldo@hotmail.com>
2024-05-13 21:15:47 +0200

    added new stimulus controller, toggling display of movie update form

---
+++ app/javascript/controllers/edit_movie_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="edit-movie"
+export default class extends Controller {
+  static targets = ["infos", "form"]
+
+  connect() {
+
+  }
+
+  displayForm() {
+    this.infosTarget.classList.add("d-none")
+    this.formTarget.classList.remove("d-none")
+  }
+}
